549282084124260f29d13be9b4289a948bfd5eb4
Esteban SP <poesteba@amazon.com>
2018-05-02 14:47:54 -0600

    Phones Java testing

---
+++ PhoneAssignment/src/com/poesteba/phones/Ringable.java
@@ -0,0 +1,8 @@
+package com.poesteba.phones;
+
+public interface Ringable {
+//		ring(): This method returns a String.
+//		unlock(): This method returns a String.
+	String ring();
+	String unlock();
+}
